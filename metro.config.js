2d05a994d17f4042f406d3afbb7ac7151cc47cb5
Carlos Cesar Feitosa dos Santos <ccesar.santos@dataprev.gov.br>
2026-03-16 11:25:32 -0300

    feat: add some libs and permission configuration

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -10,10 +11,19 @@ config.transformer = {
   ...transformer,
   babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
 };
+
 config.resolver = {
   ...resolver,
   assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...resolver.sourceExts, "svg"]
+  sourceExts: [...resolver.sourceExts, "svg"],
+  nodeModulesPaths: [
+    path.resolve(__dirname, 'node_modules'),
+    path.resolve(__dirname, '../inji-wallet-sdk/vc-sdk-headless/node_modules'),
+  ],
 };
+
+config.watchFolders = [
+  path.resolve(__dirname, '../inji-wallet-sdk/vc-sdk-headless'),
+];
 
 module.exports = config;
