2329255606e79bb22eb016a872994f5f9ae376f0
Carlos Cesar Feitosa dos Santos <ccesar.santos@dataprev.gov.br>
2026-03-24 15:15:57 -0300

    feat: add clear and get all auth data from secure storage

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { getAuthDataFromStorage, clearAllAuthDataFromStorage } from '../components/CustomAuthWebView/authStorage';
 
 interface User {
   sub?: string;
@@ -14,15 +15,27 @@ interface AuthState {
   user: User | null;
   accessToken: string | null;
   login: (authData: { user: User; accessToken: string }) => void;
+  logout: () => Promise<void>;
+  hydrate: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
   user: null,
   accessToken: null,
-  login: (authData) => set({ 
-    isAuthenticated: true, 
-    user: authData.user, 
-    accessToken: authData.accessToken 
+  login: (authData) => set({
+    isAuthenticated: true,
+    user: authData.user,
+    accessToken: authData.accessToken,
   }),
+  logout: async () => {
+    await clearAllAuthDataFromStorage();
+    set({ isAuthenticated: false, user: null, accessToken: null });
+  },
+  hydrate: async () => {
+    const data = await getAuthDataFromStorage();
+    if (data?.token?.accessToken && data?.user) {
+      set({ isAuthenticated: true, user: data.user, accessToken: data.token.accessToken });
+    }
+  },
 }));
